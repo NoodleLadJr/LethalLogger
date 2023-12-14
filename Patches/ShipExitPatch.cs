97673ae328ef42c7f52bf3f3f1d9388a42171a7e
NoodleladJr <bbrantingham@gmail.com>
2023-12-13 16:48:40 -0800

    Initial commit

---
+++ Patches/ShipExitPatch.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalLogger.Patches
+{
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class ShipExitPatch
+    {
+        [HarmonyPatch("ShipHasLeft")]
+        [HarmonyPrefix]
+        static void PrintoutExitPatch( StartOfRound __instance) {
+            ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("LethalLogger");
+            logger.LogInfo("Adniel Hamed");
+            //set to true to get collected, missed will be the diff
+            logger.LogInfo("Current Scrap Value is" + __instance.GetValueOfAllScrap(true) + " out of a max " + __instance.GetValueOfAllScrap());
+            
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalLogger.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalLogger
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LethalLoggerBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Garageman.LethalLogger";
+        private const string modName = "LC Tutorial";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static LethalLoggerBase Instance;
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null) { Instance = this; }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("LethalLogger is awake");
+            harmony.PatchAll(typeof(LethalLoggerBase));
+            harmony.PatchAll(typeof(ShipExitPatch));
+        }
+        
+
+    }
+}
