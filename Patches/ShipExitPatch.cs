44bd81131c63181140d5615ec3adbfff81d358d1
NoodleladJr <bbrantingham@gmail.com>
2023-12-14 08:59:01 -0800

    move to class with info

--- Patches/ShipExitPatch.cs
+++ Patches/ShipExitPatch.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx.Logging;
 using GameNetcodeStuff;
 using HarmonyLib;
+using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +14,45 @@ namespace LethalLogger.Patches
     [HarmonyPatch(typeof(StartOfRound))]
     internal class ShipExitPatch
     {
+
+        public class RoundInfo
+        {
+            public int living;
+            public int dead;
+            public int quota;
+            public int scrapMax;
+            public int scrapReal;
+            public int daysRemaining;
+            public String planetName;
+            public String weather;
+            public IDictionary<String, int> gear;
+            public IDictionary<String, String> playerStatus;   
+
+        }
+
         [HarmonyPatch("ShipHasLeft")]
         [HarmonyPrefix]
         static void PrintoutExitPatch( StartOfRound __instance) {
+
+            RoundInfo roundInfo = new RoundInfo();
+
             ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("LethalLogger");
             logger.LogInfo("Adniel Hamed");
             //set to true to get collected, missed will be the diff
-            int dead_players = 0;
-            int living_players = 0;
-            IDictionary<String,int> gear = new Dictionary<String,int>(); 
+
 
             foreach (PlayerControllerB playercontrollerb in GameObject.FindObjectsOfType<PlayerControllerB>())
             {
                 if (playercontrollerb != null && (playercontrollerb.isPlayerControlled || playercontrollerb.isPlayerDead))
                 {
-                    if (playercontrollerb.isPlayerDead) { dead_players++; logger.LogInfo(playercontrollerb.causeOfDeath); } else { living_players++; }
-                    logger.LogInfo("Studying: " + playercontrollerb.playerUsername);
+                    if (playercontrollerb.isPlayerDead) {
+                        roundInfo.dead++;
+                        roundInfo.playerStatus[playercontrollerb.playerUsername] = playercontrollerb.causeOfDeath.ToString();
+                        logger.LogInfo(playercontrollerb.causeOfDeath); 
+                    } else { 
+                        roundInfo.living++;
+                        roundInfo.playerStatus[playercontrollerb.playerUsername] = "Alive";
+                    }
                 }
 
             }
@@ -38,20 +62,20 @@ namespace LethalLogger.Patches
                 if (grabbable != null && grabbable.scrapValue == 0)
                 {
                     logger.LogInfo(grabbable.GetType().Name);
-                    if (gear.ContainsKey(grabbable.GetType().Name)) { gear[grabbable.GetType().Name] += 1; }
+                    if (roundInfo.gear.ContainsKey(grabbable.GetType().Name)) { roundInfo.gear[grabbable.GetType().Name] += 1; }
                     else
                     {
-                        gear[grabbable.GetType().Name] = 1;
+                        roundInfo.gear[grabbable.GetType().Name] = 1;
                     }
                 }
             }
-
-            logger.LogInfo("Living: " + living_players + " dead: " + dead_players);
-            logger.LogInfo("Current Scrap Value is " + __instance.GetValueOfAllScrap(true) + " out of a max " + __instance.GetValueOfAllScrap(false));
-            logger.LogInfo("Planet is: " + __instance.currentLevel.PlanetName);
-            logger.LogInfo("Planet Weather was: " + __instance.currentLevel.currentWeather);
+            roundInfo.scrapMax = __instance.GetValueOfAllScrap(false);
+            roundInfo.scrapReal = __instance.GetValueOfAllScrap(true);
+            roundInfo.planetName = __instance.currentLevel.PlanetName;
+            roundInfo.weather = __instance.currentLevel.PlanetName;
             TimeOfDay timeOfDay = UnityEngine.Object.FindObjectOfType<TimeOfDay>();
-            logger.LogInfo("Quota is " +  timeOfDay.profitQuota);
+            roundInfo.quota = timeOfDay.profitQuota;
+            roundInfo.daysRemaining = timeOfDay.daysUntilDeadline;
             //count dead players done
             //get each player controller and get the cause of death done
             //get player names done
@@ -60,6 +84,7 @@ namespace LethalLogger.Patches
             //get planet done
             //get gear
             //get current quota
+            //get unlockables
             //write file
 
         }
